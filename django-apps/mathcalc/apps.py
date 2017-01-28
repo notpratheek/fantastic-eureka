051447d747301e24ec8f18a68b0066e057ae0218
Pratheek <pratheek.i@gmail.com>
2017-01-28 21:42:51 +0530

    Removed to re-add: mathcalc

--- django-apps/mathcalc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MathcalcConfig(AppConfig):
-    name = 'mathcalc'
--- django-apps/mathcalc/calcs.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def _circle_area(r):
-    return math.pi * (r ** 2)
-
-def _rectangle_perimeter(l, b):
-    return 2 * (l+b)
